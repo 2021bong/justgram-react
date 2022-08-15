33a4c55f6edc4c2bb0e71331264581812d354dae
Bong <won950705@gmail.com>
2022-08-15 20:36:59 +0900

    🚧 Add : Login 컴포넌트 기능구현 - Login 컴포넌트 유효성 검사 후 로그인 버튼 활성화 기능구현 - Router로 엔드포인트별로 컴포넌트 렌더

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,57 @@
-const uidInput = document.getElementById('uid');
-const upwInput = document.getElementById('upw');
-const loginBtn = document.getElementById('login-btn');
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import LoginInput from './LoginInput';
 
-function ableLoginBtn() {
-  let idValue = uidInput.value;
-  let pwValue = upwInput.value;
-  let isValid =
-    idValue.includes('@') && idValue.length >= 5 && pwValue.length >= 5;
+const Login = () => {
+  const [disabled, setDisabled] = useState(true);
+  const [idValue, setIdValue] = useState();
+  const [pwValue, setPwValue] = useState();
+  const idInputRef = useRef('');
+  const pwInputRef = useRef('');
 
-  loginBtn.disabled = !isValid;
-}
+  const ableLoginBtn = (s) => {
+    let idValue = idInputRef.current.value;
+    let pwValue = pwInputRef.current.value;
+    setIdValue(idValue);
+    setPwValue(pwValue);
+    if (idValue.includes('@') && idValue.length >= 5 && pwValue.length >= 5) {
+      setDisabled((prev) => !prev);
+    }
+  };
 
-uidInput.addEventListener('input', ableLoginBtn);
-upwInput.addEventListener('input', ableLoginBtn);
-loginBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.open('./main.html', '_self');
-});
+  return (
+    <div className="container flex-center">
+      <div className="login-container">
+        <h1 className="login-title">justgram</h1>
+        <form className="login-form">
+          <LoginInput
+            id="uid"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            onAbleLoginBtn={ableLoginBtn}
+            ref={idInputRef}
+          ></LoginInput>
+          <LoginInput
+            id="upw"
+            type="password"
+            placeholder="비밀번호"
+            onAbleLoginBtn={ableLoginBtn}
+            ref={pwInputRef}
+          ></LoginInput>
+          <Link to="/">
+            <button id="login-btn" disabled={disabled}>
+              로그인
+            </button>
+          </Link>
+        </form>
+        <div className="flex-center">
+          <a className="find-pw" href="#">
+            비밀번호를 잊으셨나요?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
