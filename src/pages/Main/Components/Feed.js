c078a5928702c640e01057b13f40bc60ebe58a7c
Bong <won950705@gmail.com>
2022-08-19 16:25:01 +0900

    ♻️ Refactor : Comments 컴포넌트 삭제 - 불필요한 Comments 컴포넌트 삭제 - Feed에서 Comment 컴포넌트 바로 호출하도록 변경

--- src/pages/Main/Components/Feed.js
+++ src/pages/Main/Components/Feed.js
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from 'react';
-import Comments from './Comments';
-import styles from '../Main.module.scss';
+import React, { useRef, useState } from "react";
+import Comment from "./Comment";
+import styles from "../Main.module.scss";
 
 const Feed = ({
   userName,
@@ -13,17 +13,17 @@ const Feed = ({
 }) => {
   const [disabled, setDisabled] = useState(true);
   const [id, setid] = useState(3);
-  const [commentInputValue, setCommentInputValue] = useState('');
+  const [commentInputValue, setCommentInputValue] = useState("");
   const [comments, setComments] = useState([
     {
       id: 1,
-      userId: '2021bong',
-      content: '공부를 합시다!',
+      userId: "2021bong",
+      content: "공부를 합시다!",
     },
-    { id: 2, userId: 'happy2022', content: 'good :)' },
+    { id: 2, userId: "happy2022", content: "good :)" },
   ]);
   const [like, setLike] = useState(true);
-  const [heartUrl, setHeartUrl] = useState('images/heart_empty.png');
+  const [heartUrl, setHeartUrl] = useState("images/heart_empty.png");
 
   const commentRef = useRef();
 
@@ -36,16 +36,16 @@ const Feed = ({
     setid(id + 1);
     const newComment = {
       id,
-      userId: 'guest1',
+      userId: "guest1",
       content: commentInputValue,
     };
 
     setComments((prevComments) => [...prevComments, newComment]);
-    commentRef.current.value = '';
+    commentRef.current.value = "";
   };
 
   const deleteComment = (e) => {
-    if (window.confirm('정말 삭제 하시겠습니까?')) {
+    if (window.confirm("정말 삭제 하시겠습니까?")) {
       const deleteIndex = e.nativeEvent.target.id;
       setComments((prevComments) => {
         const deletedComments = prevComments.filter((comment) => {
@@ -64,14 +64,14 @@ const Feed = ({
 
   const likeFeed = () => {
     setLike((like) => !like);
-    setHeartUrl(like ? 'images/heart_full.png' : 'images/heart_empty.png');
+    setHeartUrl(like ? "images/heart_full.png" : "images/heart_empty.png");
   };
 
   return (
     <section
       className={`${styles.feed}`}
       onKeyDown={(e) => {
-        if (e.key === 'Enter') {
+        if (e.key === "Enter") {
           if (e.nativeEvent.isComposing === false) {
             e.preventDefault();
             addComment();
@@ -79,85 +79,97 @@ const Feed = ({
         }
       }}
     >
-      <div className={`${styles['feed-id']} vertical-center`}>
-        <div className="vertical-center">
-          <div className={`${styles['profile-cur']}`}></div>
+      <div className={`${styles["feed-id"]} vertical-center`}>
+        <div className='vertical-center'>
+          <div className={`${styles["profile-cur"]}`}></div>
 
-          <span className={`${styles['feed-id-uid']} bold`}>{userName}</span>
+          <span className={`${styles["feed-id-uid"]} bold`}>{userName}</span>
         </div>
 
         <img
-          className="medium-icon-setting"
-          src="/images/option.png"
-          alt="더보기 아이콘"
+          className='medium-icon-setting'
+          src='/images/option.png'
+          alt='더보기 아이콘'
         />
       </div>
-      <div className={`${styles['feed-img-container']}`}>
-        <div className={`${styles['feed-img']} flex-center`}>
+      <div className={`${styles["feed-img-container"]}`}>
+        <div className={`${styles["feed-img"]} flex-center`}>
           <img className={`${styles.img}`} src={imageSrc} alt={imageAlt} />
         </div>
-        <div className={`${styles['feed-img-menu']}`}>
-          <ul className="vertical-center">
-            <div className={`${styles['img-menu-group']} vertical-center`}>
+        <div className={`${styles["feed-img-menu"]}`}>
+          <ul className='vertical-center'>
+            <div className={`${styles["img-menu-group"]} vertical-center`}>
               <li onClick={likeFeed}>
-                <img className="icon-setting" alt="좋아요" src={heartUrl} />
+                <img className='icon-setting' alt='좋아요' src={heartUrl} />
               </li>
               <li>
                 <img
-                  className="icon-setting"
-                  alt="댓글"
-                  src="/images/chat.png"
+                  className='icon-setting'
+                  alt='댓글'
+                  src='/images/chat.png'
                 />
               </li>
               <li>
                 <img
-                  className="icon-setting"
-                  alt="공유"
-                  src="/images/message_icon.png"
+                  className='icon-setting'
+                  alt='공유'
+                  src='/images/message_icon.png'
                 />
               </li>
             </div>
             <li>
               <img
-                className="icon-setting"
-                alt="저장"
-                src="/images/bookmark.png"
+                className='icon-setting'
+                alt='저장'
+                src='/images/bookmark.png'
               />
             </li>
           </ul>
         </div>
       </div>
-      <div className={`${styles['feed-cont-container']}`}>
+      <div className={`${styles["feed-cont-container"]}`}>
         <p
-          className={`bold ${styles['like-count']}`}
+          className={`bold ${styles["like-count"]}`}
         >{`좋아요 ${likeCount}개`}</p>
-        <p className={`${styles['feed-cont']}`}>
-          <span className={`bold mr5 ${['feed-cont-uid']}`}>{userName}</span>
-          <span className={`${styles['feed-cont-com']}`}>{content}</span>
-          <span className="color-gray ml5 pointer">더 보기</span>
+        <p className={`${styles["feed-cont"]}`}>
+          <span className={`bold mr5 ${["feed-cont-uid"]}`}>{userName}</span>
+          <span className={`${styles["feed-cont-com"]}`}>{content}</span>
+          <span className='color-gray ml5 pointer'>더 보기</span>
         </p>
-        <span className="color-gray pointer">{`댓글 ${allComment}개 모두 보기`}</span>
-        <Comments propsComments={comments} deleteComment={deleteComment} />
+        <span className='color-gray pointer'>{`댓글 ${allComment}개 모두 보기`}</span>
+        <ul className={`${styles["new-com-container"]}`}>
+          {comments.map((comment) => {
+            return (
+              <Comment
+                key={comment.id}
+                id={comment.id}
+                userId={comment.userId}
+                content={comment.content}
+                deleteComment={deleteComment}
+              />
+            );
+          })}
+        </ul>
         <p
-          className={`${styles['feed-cont-time']} color-gray`}
+          className={`${styles["feed-cont-time"]} color-gray`}
         >{`${createdTime}시간전`}</p>
       </div>
-      <div className={`${styles['feed-com']} vertical-center`}>
-        <div className={`${styles['smile-icon']}`}></div>
+      <div className={`${styles["feed-com"]} vertical-center`}>
+        <div className={`${styles["smile-icon"]}`}></div>
         <form
-          className={`${styles['feed-com-form']} vertical-center`}
+          className={`${styles["feed-com-form"]} vertical-center`}
           onSubmit={blockRefresh}
         >
           <input
-            className={`${styles['comment-input']}`}
-            type="text"
-            placeholder="댓글 달기..."
+            className={`${styles["comment-input"]}`}
+            type='text'
+            placeholder='댓글 달기...'
             onChange={writeComment}
             ref={commentRef}
           />
           <button
-            type="button"
-            className={`${styles['comment-btn']}`}
+            type='button'
+            className={`${styles["comment-btn"]}`}
             onClick={addComment}
             disabled={disabled}
           >
